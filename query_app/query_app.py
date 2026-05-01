6491905de270c6acf1d269b74c3b2d692b345aa8
Tony Ibbs (Tibs) <tony.ibbs@aiven.io>
2026-05-01 18:06:09 +0100

    Make the query app report if the CLIP app is not ready

--- query_app/query_app.py
+++ query_app/query_app.py
@@ -14,6 +14,7 @@ import httpx
 
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request, Form
+from fastapi import status
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -43,8 +44,44 @@ MODEL_NAME = os.environ.get('MODEL_NAME', 'openai/clip-vit-base-patch32')
 # Get the URL for our CLIP embedding service
 CLIP_SERVICE_URL = os.environ.get('CLIP_SERVICE_URL', 'http://localhost:8000')
 
-# Out table name
+# Our table name
 TABLE_NAME = 'pictures'
+
+
+async def clip_service_is_ready() -> False:
+    """Is the CLIP service ready to serve embeddings?
+    """
+    logger.info('Checking CLIP service')
+    try:
+        response = httpx.get(
+            f'{CLIP_SERVICE_URL}/healthy',
+            timeout=None,    # the default is documented as 5 seconds
+        )
+        if response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE:
+            return False
+
+        logger.info('CLIP service is ready')
+        return True
+
+    except Exception as exc:
+        logger.error(f'Error getting CLIP service readiness from {CLIP_SERVICE_URL}: {exc.__class__.__name__}: {exc}')
+        # raise Exception(f'Error getting CLIP service readiness from {CLIP_SERVICE_URL}: {exc.__class__.__name__}: {exc}')
+        return False
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Define events at the start and end of the app lifespan
+
+    This means we can start the query app before the CLIP service ready,
+    and still give _some_ response to the user
+    """
+    logger.info('Async load task starting')
+    blocking_loader = asyncio.to_thread(wait_for_clip_model)
+    asyncio.create_task(blocking_loader)
+    yield
+    # We don't have an unload step
+
 
 async def get_text_embedding(text) -> List[float]:
     logger.info(f'Requesting embeddings for {text}')
@@ -118,6 +155,25 @@ async def index(request: Request):
 @app.post("/search_form", response_class=HTMLResponse)
 async def search_form(request: Request, search_text: Annotated[str, Form()]):
     logging.info(f'Search form requests {search_text!r}')
+
+    if not await clip_service_is_ready():
+        return templates.TemplateResponse(
+            request=request,
+            name="images.html",
+            context={
+                "images": [],
+                "error_message": "CLIP service is not ready to serve embeddings yet - please try again later",
+            }
+        )
+
+    # It would also be nice to be able to check if the database is populated yet.
+    # We _could_ COUNT the records in the database, and report how many / complain if
+    # there aren't enough, or we _could_ make the setup_db callable into another
+    # service, and then we could ask it. Leaving that running forever when it's only
+    # got one thing to do might seem excessive, but if there's a service to add new
+    # images (and their embeddings) to the database, then we could use that to add
+    # the capability of adding more images to _this_ app.
+    # For now, we'll just ignore the problem for the moment...
 
     try:
         results = await search_for_matches(search_text)
